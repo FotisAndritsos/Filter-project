e7935a4ae699f9a2ac16d5233f8bdbb9dcd5c64c
FotisAndritsos <fotisandritsos85@gmail.com>
2020-10-30 23:35:09 +0200

    loading the products

--- main.js
+++ main.js
@@ -2,49 +2,70 @@ let products = [
     {
         id:1,
         desc:"Nike Air max",
-        img:"/images/revolt-164_6wVEHfI-unsplash.jpg",
-        price:170
+        img:"/images/sharon-mccutcheon-wx3JOq0Xbh4-unsplash.jpg",
+        category:"Tshirt",
+        price:170,
     },
     {
         id:2,
         desc:"Nike Air max super",
-        img :"/images/revolt-164_6wVEHfI-unsplash.jpg",
-        price:122
+        img :"/images/marcel-j-uWrCdB5sM-unsplash.jpg",
+        category:"Tshirt",
+        price:122,
     },
     {
         id:3,
         desc:"Logo nik T-shirt",
         img:"/images/revolt-164_6wVEHfI-unsplash.jpg",
-        price:17
+        category:"Shoes",
+        price:17,
     },
     {
         id:4,
         desc:"Jordn nik T-shirt",
-        img:"/images/revolt-164_6wVEHfI-unsplash.jpg",
-        price:27
+        img:"/images/jeremy-alford-EfLwt5Xz5Ek-unsplash.jpg",
+        category:"Shoes",
+        price:27,
     },
     {
         id:5,
         desc:"Caual shirt",
         img:"/images/revolt-164_6wVEHfI-unsplash.jpg",
-        price:47
+        category:"Shirt",
+        price:47,
     },
     {
         id:6,
-        desc:"Shirt on and go";
+        desc:"Shirt on and go",
         img:"/images/revolt-164_6wVEHfI-unsplash.jpg",
-        price:67
+        category:"Shirt",
+        price:67,
     }
     
 ]
 const productContainer = document.querySelector('.product-container');
+const btns = document.querySelectorAll('button');
 
 window.addEventListener('DOMContentLoaded', () => {
-    let yo = products.map(item => {
-        return '<div class="product"> <div class="img-container"><img src = ${product.img} alt=""></div><div class="main-body"><h3>${product.desc}</h3><p>Price<span>${product.price}&#8364</span></p></div></div>'
-    
-    });
+    let yo = products.map(product => {
+        return`<div class="product">
+                    <div class="img-container">
+                    <img src="${product.img}" alt="">
+                    </div>
+                    <div class="main-body">
+                        <h3>${product.desc}</h3>
+                        <p>Price<span> ${product.price}&#8364</span></p>
+                    </div>
+                </div>
+             `;
+});
         // console.log(yo);
         yo = yo.join("");
         productContainer.innerHTML = yo;
+});
+btns.forEach(btn => {
+    btn.addEventListener('click',(e)=>{
+        let targItem = e.target.dataset.id;
+        
+    })
 });
